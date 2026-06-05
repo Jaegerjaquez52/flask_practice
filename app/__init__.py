26822fc932c9f73f8347d9518689446f2f173b07
Jaegerjaquez52 <andriy007polischuk@gmail.com>
2026-06-05 11:51:35 +0300

    Email Support

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from logging.handlers import SMTPHandler, RotatingFileHandler
+from flask_mail import Mail
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,7 +14,9 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
 
+from app import routes, models, errors
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -41,5 +45,3 @@ if not app.debug:
 
         app.logger.setLevel(logging.INFO)
         app.logger.info('Microblog startup')
-
-from app import routes, models, errors
--- app/models.py
+++ app/models.py
@@ -7,6 +7,9 @@ import sqlalchemy.orm as so
 from app import db
 from app import login
 from hashlib import md5
+from time import time
+import jwt
+from app import app
 
 @login.user_loader
 def load_user(id):
@@ -92,6 +95,20 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return db.session.get(User, id)
 
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
