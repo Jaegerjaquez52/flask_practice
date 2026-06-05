91178a5186326f6a518e3eb7543e82cada10e39c
Jaegerjaquez52 <andriy007polischuk@gmail.com>
2026-06-05 14:10:22 +0300

    LAnguages

--- app/models.py
+++ app/models.py
@@ -1,19 +1,14 @@
-from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timezone
-from typing import Optional
-from flask_login import UserMixin
-import sqlalchemy as sa
-import sqlalchemy.orm as so
-from app import db
-from app import login
 from hashlib import md5
 from time import time
+from typing import Optional
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
-from app import app
+from app import app, db, login
 
-@login.user_loader
-def load_user(id):
-    return db.session.get(User, int(id))
 
 followers = sa.Table(
     'followers',
@@ -24,6 +19,7 @@ followers = sa.Table(
               primary_key=True)
 )
 
+
 class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
@@ -31,12 +27,12 @@ class User(UserMixin, db.Model):
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
                                              unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
-
-    posts: so.WriteOnlyMapped['Post'] = so.relationship(
-        back_populates='author')
     about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
     last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
         default=lambda: datetime.now(timezone.utc))
+
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(
+        back_populates='author')
     following: so.WriteOnlyMapped['User'] = so.relationship(
         secondary=followers, primaryjoin=(followers.c.follower_id == id),
         secondaryjoin=(followers.c.followed_id == id),
@@ -45,6 +41,19 @@ class User(UserMixin, db.Model):
         secondary=followers, primaryjoin=(followers.c.followed_id == id),
         secondaryjoin=(followers.c.follower_id == id),
         back_populates='following')
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
     def follow(self, user):
         if not self.is_following(user):
@@ -83,19 +92,6 @@ class User(UserMixin, db.Model):
             .order_by(Post.timestamp.desc())
         )
 
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-    def avatar(self, size):
-        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
-        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
-
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
-
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
             {'reset_password': self.id, 'exp': time() + expires_in},
@@ -106,9 +102,15 @@ class User(UserMixin, db.Model):
         try:
             id = jwt.decode(token, app.config['SECRET_KEY'],
                             algorithms=['HS256'])['reset_password']
-        except:
+        except Exception:
             return
         return db.session.get(User, id)
+
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
+
 
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
