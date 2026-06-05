ce92f016c374381d1a79105ddbc3a2d1e66c834f
Jaegerjaquez52 <andriy007polischuk@gmail.com>
2026-06-05 15:08:23 +0300

    Ajax

--- app/routes.py
+++ app/routes.py
@@ -9,7 +9,8 @@ from app.forms import LoginForm, RegistrationForm, EditProfileForm, \
     EmptyForm, PostForm, ResetPasswordRequestForm, ResetPasswordForm
 from app.models import User, Post
 from app.email import send_password_reset_email
-
+from langdetect import detect, LangDetectException
+from app.translate import translate
 
 @app.before_request
 def before_request():
@@ -19,13 +20,19 @@ def before_request():
     g.locale = str(get_locale())
 
 
+
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
+        try:
+            language = detect(form.post.data)
+        except LangDetectException:
+            language = ''
+        post = Post(body=form.post.data, author=current_user,
+                    language=language)
         db.session.add(post)
         db.session.commit()
         flash(_('Your post is now live!'))
@@ -206,3 +213,9 @@ def unfollow(username):
         return redirect(url_for('user', username=username))
     else:
         return redirect(url_for('index'))
+
+@app.route('/translate', methods=['POST'])
+@login_required
+def translate_text():
+    data = request.get_json()
+    return {'text': translate(data['text'], data['source_language'], data['dest_language'])}
